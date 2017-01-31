782b67ca1216cfb4e3d8b0c4c06a4b529cd42e19
ITLAB <ITLAB@IT217>
2017-01-31 15:51:38 +0530

    fifth

--- src/com/example/calc/MainActivity.java
+++ src/com/example/calc/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
  
     Button button0 , button1 , button2 , button3 , button4 , button5 , button6 ,
             button7 , button8 , button9 , buttonAdd , buttonSub , buttonDivision ,
-            buttonMul , button11 ,button10, buttonC , buttonEqual ;
+            buttonMul , button10 , buttonC , buttonEqual ;
  
     EditText edt1 ;
  
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
  
-        button0 = (Button) findViewById(R.id.button11);
+        button0 = (Button) findViewById(R.id.button0);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
@@ -33,14 +33,14 @@ public class MainActivity extends Activity {
         button7 = (Button) findViewById(R.id.button7);
         button8 = (Button) findViewById(R.id.button8);
         button9 = (Button) findViewById(R.id.button9);
-        button10 = (Button) findViewById(R.id.button17);
-        buttonAdd = (Button) findViewById(R.id.button10);
-        buttonSub = (Button) findViewById(R.id.button14);
-        buttonMul = (Button) findViewById(R.id.button15);
-        buttonDivision = (Button) findViewById(R.id.button16);
-        buttonC = (Button) findViewById(R.id.button18);
-        buttonEqual = (Button) findViewById(R.id.button12);
-        edt1 = (EditText) findViewById(R.id.editText1);
+        button10 = (Button) findViewById(R.id.button10);
+        buttonAdd = (Button) findViewById(R.id.buttonadd);
+        buttonSub = (Button) findViewById(R.id.buttonsub);
+        buttonMul = (Button) findViewById(R.id.buttonmul);
+        buttonDivision = (Button) findViewById(R.id.buttondiv);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonEqual = (Button) findViewById(R.id.buttoneql);
+        edt1 = (EditText) findViewById(R.id.edt1);
  
  
         button1.setOnClickListener(new View.OnClickListener() {
@@ -197,4 +197,5 @@ public class MainActivity extends Activity {
             }
         });
     }
+ 
 }
